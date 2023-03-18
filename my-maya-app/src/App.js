2c73926557aac31d747a149e733395adbbc3de16
Stephan <stephan@fazemodels.com>
2023-03-18 15:22:02 +0100

    Integrate Script

--- my-maya-app/src/App.js
+++ my-maya-app/src/App.js
@@ -1,6 +1,64 @@
 import React, { useState } from "react";
 import "./App.css";
 
+function getDreamspellKin(date) {
+  const gregorianStartDate = new Date(1853, 6, 26); // July 26, 1853
+  const dreamspellStartDate = new Date(1853, 6, 26); // July 26, 1853
+
+  const daysDifference = Math.floor(
+    (date - gregorianStartDate) / (1000 * 60 * 60 * 24)
+  );
+  const kinNumber = ((daysDifference % 260) + 260) % 260;
+
+  const toneNames = [
+    "Magnetic",
+    "Lunar",
+    "Electric",
+    "Self-existing",
+    "Overtone",
+    "Rhythmic",
+    "Resonant",
+    "Galactic",
+    "Solar",
+    "Planetary",
+    "Spectral",
+    "Crystal",
+    "Cosmic",
+  ];
+
+  const solarSealNames = [
+    "Dragon",
+    "Wind",
+    "Night",
+    "Seed",
+    "Serpent",
+    "Worldbridger",
+    "Hand",
+    "Star",
+    "Moon",
+    "Dog",
+    "Monkey",
+    "Human",
+    "Skywalker",
+    "Wizard",
+    "Eagle",
+    "Warrior",
+    "Earth",
+    "Mirror",
+    "Storm",
+    "Sun",
+  ];
+
+  const tone = (kinNumber % 13) + 1;
+  const solarSeal = kinNumber % 20;
+
+  return {
+    kinNumber: kinNumber + 1,
+    toneName: toneNames[tone - 1],
+    solarSealName: solarSealNames[solarSeal],
+  };
+}
+
 function App() {
   const [selectedDate, setSelectedDate] = useState("");
   const [results, setResults] = useState(null);
@@ -10,9 +68,11 @@ function App() {
   };
 
   const handleSubmit = () => {
-    // Call your API or process the date and display the results
-    // For example:
-    setResults(`You have selected: ${selectedDate}`);
+    const date = new Date(selectedDate);
+    const dreamspellKin = getDreamspellKin(date);
+    setResults(
+      `Kin ${dreamspellKin.kinNumber}: ${dreamspellKin.toneName} ${dreamspellKin.solarSealName}`
+    );
   };
 
   return (
